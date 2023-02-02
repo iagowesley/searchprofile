958c0910c678f0870c62e831a89094cecedf4096
Iago Wesley Irineu Costa <iago.costa@fitbank.com.br>
2023-02-02 15:22:17 -0300

    search

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+type GITHUBResponse = {
+  name: string;
+  avatar_url: string;
+  bio: string;
+};
+
+function App() {
+  const [userName, setUserName] = useState("");
+  const [name, setName] = useState("Aguardando...");
+  const [bio, setBio] = useState("Aguardando...");
+  const [avatarURL, setAvatarURL] = useState("Aguardando...");
+
+  const handleSearch = () => {
+    axios
+      .get<GITHUBResponse>(`https://api.github.com/users/${userName}`)
+      .then((res) => {
+        setName(res.data.name);
+        setBio(res.data.bio);
+        setAvatarURL(res.data.avatar_url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="container-app">
+      <div className="container">
+        
+        <main>
+          <div className="form">
+            <h1>Buscador de Perfis do GITHUB</h1>
+            <input
+              type="text"
+              placeholder="Digite um username"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <button onClick={handleSearch}>Buscar</button>
+          </div>
+          <div className="content">
+            <div>
+              <img src={avatarURL} alt="Perfil" />
+              <h1>{name}</h1>
+              <a href={"https://github.com/" + userName}>LinkPerfil</a>
+
+            </div>
+          </div>
+
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
